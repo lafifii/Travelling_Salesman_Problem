b81c28f943231c55587d9a4c3aaf0a96c8286637
francoMG <francomolocheg99@gmail.com>
2018-09-22 15:06:01 -0500

    Update main.js

--- app/client/js/main.js
+++ app/client/js/main.js
@@ -13,14 +13,14 @@ function process(text) {
 function setup() {
 
   noCanvas();
-  // Check for the various File API support.
+  
   if (window.File && window.FileReader && window.FileList && window.Blob) {
     console.log('Great success! All the File APIs are supported');
   } else {
     alert('The File APIs are not fully supported in this browser.');
   }
-
-  $.ajax({                        /// deeeeeeeeeeeeeeeleete this
+  
+  $.ajax({
     url: "http://localhost:80",
     type: "POST",
     data: event.target.result,
@@ -28,71 +28,13 @@ function setup() {
     success: function(data) {
       jsonTest = data;
       selectableForceDirectedGraph(data);
-  }});                        /// deeeeeeeeeeeeeeel
-
-  // <div id="drop_zone">Arraste el archivo aqui
-  // Make a div to drag a file on
-  var dropZone = createDiv('<p align="center"> >\Arraste el archivo aqui <\ </p>');
-  dropZone.id('drop_zone');
-  dropZone.style('background','#EEEEEE');
-
-  // A description of files
-  var dfile = createDiv();
-  dfile.id('dfile');
-
+  }});
+  
+  var txt = createDiv('<p align="center"> >\Explicación <\ </p>');
+  txt.style('background','#EEEEEE');
+  
   // This div is create to the graph
   var nd = createDiv();
   nd.id('d3_selectable_force_directed_graph');
   nd.style('align', 'center');
-
-  // Add some events
-  dropZone.elt.addEventListener('dragover', handleDragOver, false);
-  dropZone.elt.addEventListener('drop', handleFileSelect, false);
-  dropZone.elt.addEventListener('dragleave', handleDragLeave, false);
-
-  // When you drag a file on top
-  function handleDragOver(evt) {
-    // Stop the default browser behavior
-    evt.stopPropagation();
-    evt.preventDefault();
-    dropZone.style('background','#AAAAAA');
-  }
-
-  // If the mosue leaves
-  function handleDragLeave(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-  }
-
-  // If you drop the file
-  function handleFileSelect(evt) {
-
-    evt.stopPropagation();
-    evt.preventDefault();
-    dropZone.style('background','#EEEEEE');
-
-    var f = evt.dataTransfer.files[0];
-
-    if ( f.type == 'text/plain'){
-
-      document.getElementById('dfile').innerHTML = ('Nombre: [' + f.name + '] Tipo: [' + f.type + '] Tamaño: [' + f.size + ']');
-      reader = new FileReader();
-      reader.onload = function(event) {
-
-        $.ajax({
-          url: "http://localhost:80",
-          type: "POST",
-          data: event.target.result,
-          dataType: "text",
-          success: function(data) {
-            jsonTest = data;
-            selectableForceDirectedGraph(data);
-        }});
-      }
-      reader.readAsBinaryString(f);
-    }
-    else{
-      document.getElementById('dfile').innerHTML = 'El archivo no es comprensible -> ' + f.type;
-    }
-  }
 }
