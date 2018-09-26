6d932345aece8563908047c8ffc2798223026477
Fiorella Barrientos <35583274+fiorella2709@users.noreply.github.com>
2018-09-26 03:57:54 -0500

    Update DP+Bitmasking.py

--- solucion2/DP+Bitmasking.py
+++ solucion2/DP+Bitmasking.py
@@ -3,12 +3,11 @@ import math
 
 INT_MAX  = 999999
 n=20
-#COMPLEJIDAD O(2^n * n^2) puede resolver el problema hasta lo que alcanze un 
-#long long para el mask
-#resuelve el problema hasta un 0.013 % con un tiempo prudente en este ejemplo
-#ya que cada 10^8 es un segundo aproximado con 2^8 * 8^2 tenemos 1048576 + 64 iteraciones
-#que es mas de 10^9 incluso por lo que cada 20 ciudades se demorara mas de un segundo
-#y esto crece exponencialmente muy rapido ya que se hablan de potencias de 2
+#COMPLEJIDAD maximo valor del mask * pos = 1048576 * 20 , es un algortimo eficiente
+#pero usa variables muy grandes ya que en la mascara cada 1 es una ciudad ya visitada
+# entonces si es 20 el numero de ciudades el numero del mask debe ser 
+# 2 + 4 + 8 + 16 + 32 ..... 2^20 = 1048576 y solo soporta hasta el numero maximo
+#que pueda soportar un long long osea 64 ciudades como maximo
 dist = [[0 for x in range(140)] for y in range(140)]
 todos_vis = (1<<n) -1;
 dp = [[0 for x in range(20)] for y in range(1048576)]
@@ -52,6 +51,25 @@ def tsp(mask,pos):
 	dp[mask][pos] = ans
 	return dp[mask][pos];
 
+def cal_dis(x1,y1,x2,y2):
+	return math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2))
+
+def rec(mask,pos, s): 
+	global dp,INT_MAX,todos_vis
+
+	if(mask==todos_vis):
+		return dist[pos][0]
+	
+	if(dp[mask][pos]!=-1):
+	   return dp[mask][pos]
+	
+	ans = INT_MAX;
+	for ciudad in range(n):
+		if((mask&(1<<ciudad))==0):
+			ans2 = dist[pos][ciudad] + tsp( mask|(1<<ciudad), ciudad);
+			if(ans2<ans):
+				s = s + str(ciudad)
+
 #main
 distancias = []
 csv2txt(distancias)
@@ -64,4 +82,6 @@ for i in range(1<<n): #2 a la n
 		dp[i][j] = -1;
 
 print("tsp dp + bitmasking ", tsp(1,0))
-
+s = ""
+rec(1,0,s)
+print(s)
